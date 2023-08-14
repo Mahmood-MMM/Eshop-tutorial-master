38b5f637d0262bb9e1606c3600427b1cf5d34d57
Mahmood-MMM <109480929+Mahmood-MMM@users.noreply.github.com>
2023-08-15 02:15:49 +0300

    frontend patch updated done

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://eshop-tutorial-master-indol.vercel.app/',
   credentials: true
 }));
 
